fe46e8c1a1b710da1d37cf74a3ed5c267cabd392
asbrar42 <35750752+asbrar42@users.noreply.github.com>
2021-03-23 17:40:54 -0500

    updated code

--- feRequest.php
+++ feRequest.php
@@ -1,1 +1,18 @@
-test
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('192.168.192.147', 5672, 'username', 'password');
+$channel = $connection->channel();
+
+$channel->queue_declare('request', false, false, false, false);
+
+$msg = new AMQPMessage("'$username'!'$password'!'$login'");
+$channel->basic_publish($msg, '', 'request');
+
+$channel->close();
+$connection->close();
+?>
+
